21c7f0f374b07149728d97d9ee8fafe2c60af879
Kuntalmajee2557 <kuntalmajee338@gmail.com>
2025-01-18 00:56:03 +0530

    home page done

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import BookIcon from '../components/icons/BookIcon'
 
 function Home() {
-    const navigate = useNavigate()
-  function clickHandler(){
+  const navigate = useNavigate()
+  function clickHandler() {
     navigate('/docs')
   }
   return (
-    <div>Documentation
-    <button onClick={clickHandler} className='bg-red-500'>
-      browse
-    </button>
+  <div className='h-screen bg-black flex flex-col justify-center items-center bg-[url("1111767_589.jpg")]'>
+    <BookIcon className='w-48 h-48 text-cyan-500'/>
+    <button onClick={clickHandler} className='text-xl text-white border border-gray-600 py-2 px-9 rounded-lg mt-12'>Browser Docs</button>
   </div>
   )
 }
